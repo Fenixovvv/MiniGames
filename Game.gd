cbab14cb482f7e0fe9803b29a5bea2075fa65127
Егор Туник <yfenixov@gmail.com>
2024-01-08 17:10:01 +0000

    its moving

--- Game.gd
+++ Game.gd
@@ -2,17 +2,23 @@ extends Node2D
 
 @export var car: PackedScene
 @onready var start = true
-@onready var car_instance = car.instantiate() 
+var car_instance: Sprite2D
+var hor_vector: Vector2
 
 func _ready():
 	print("READY")
 	
 func _process(delta):
 	if start:
-		owner.add_child(car_instance)
+		car_instance = car.instantiate()
+		add_child(car_instance)
 		car_instance.position = Vector2(360, 720)
 		start = false
 	
-	if Input.is_action_just_pressed("ui_left"):
-		car_instance.position += Vector2(-10, 0)
+	if Input.is_action_pressed("ui_left") and car_instance.position.x > 55:
+		hor_vector = Vector2(-10, 0)
+	elif Input.is_action_pressed("ui_right") and car_instance.position.x < 665:
+		hor_vector = Vector2(10, 0)
 	
+	car_instance.position += hor_vector
+	hor_vector = Vector2(0, 0)
